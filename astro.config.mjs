a65c338a2c323e3219b24067d1e5fa8cfc220cb6
vclemenzi <vclemenzi@users.noreply.github.com>
2024-02-10 18:46:07 +0100

    chore: cloudflare pages adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind()],
+  adapter: cloudflare({ mode: "directory" }),
 });
