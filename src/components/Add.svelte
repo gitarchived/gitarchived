91bb341a1dc66bd73ab048d10da501de83476cf9
gigantino <git@gigantino.dev>
2024-02-11 17:12:40 +0100

    feat: add new repo modal

---
+++ src/components/Add.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+  import plusIcon from "../images/plus.svg";
+  import closeIcon from "../images/x.svg";
+  import { fade } from "svelte/transition";
+  let modalEnabled = false;
+  let errorMessage = "";
+  let repoName = "";
+  let isLoading = false;
+</script>
+
+{#if modalEnabled}
+  <div
+    class="absolute left-0 top-0 flex h-screen w-full items-center justify-center bg-black/40"
+    transition:fade={{ delay: 0, duration: 100 }}
+  >
+    <div
+      class="flex h-screen w-full flex-col items-center justify-center rounded-lg bg-stone-800 p-4 sm:h-min sm:max-w-sm"
+    >
+      <div class="flex w-full justify-between">
+        <div class="text-lg font-bold">Add a new repo</div>
+        <button on:click={() => (modalEnabled = false)}>
+          <img src={closeIcon.src} alt="search" />
+        </button>
+      </div>
+      <form
+        class="flex w-full flex-col gap-2 pt-2"
+        on:submit={(e) => {
+          e.preventDefault();
+          if (isLoading) return;
+          if (!repoName || repoName === "") return (errorMessage = "Missing URL.");
+          isLoading = true;
+          console.log(repoName);
+          fetch("https://api.gitarchived.org/create", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              url: repoName,
+            }),
+          }).then(async (data) => {
+            isLoading = false;
+            const parsedData = await data.json();
+            if (parsedData.status !== 200) return (errorMessage = parsedData.message);
+            location.href = `/r/${parsedData.host}/${parsedData.owner}/${parsedData.name}`;
+          });
+        }}
+      >
+        <div class="relative">
+          <input
+            type="text"
+            bind:value={repoName}
+            placeholder="https://github.com/torvalds/linux"
+            class="h-9 w-full rounded-lg border border-stone-50/10 bg-stone-800 px-8 ring-stone-50/10 focus:outline-none focus:ring"
+          />
+          <img src={plusIcon.src} alt="search" class="absolute left-2 top-2" />
+        </div>
+        <button
+          class="h-9 w-full rounded-lg bg-[#F1502F] ring-orange-600/50 focus:outline-none focus:ring"
+        >
+          Submit
+        </button>
+        {#if errorMessage !== ""}
+          <p class="font-bold text-red-400">{errorMessage}</p>
+        {/if}
+      </form>
+    </div>
+  </div>
+{/if}
+
+<button class="cursor-pointer font-bold underline" on:click={() => (modalEnabled = true)}>
+  add a new repo
+</button>
