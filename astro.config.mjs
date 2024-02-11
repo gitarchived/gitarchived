3adb47b535749efd131e393a11a3171339f56338
gigantino <git@gigantino.dev>
2024-02-11 15:16:33 +0100

    feat: add search

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import cloudflare from "@astrojs/cloudflare";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind()],
-  adapter: cloudflare({ mode: "directory" }),
+  integrations: [tailwind(), svelte()],
+  adapter: cloudflare({
+    mode: "directory"
+  })
 });
