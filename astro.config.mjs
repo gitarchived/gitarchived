fcc8c88c2399d14d180188660e9cebbe36cf427a
vclemenzi <vclemenzi@users.noreply.github.com>
2024-02-13 18:55:16 +0100

    docs: faq page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import svelte from "@astrojs/svelte";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), mdx()],
   adapter: cloudflare({
-    mode: "directory",
-  }),
+    mode: "directory"
+  })
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  plugins: [
+    require("@tailwindcss/typography"),
+  ]
 };
